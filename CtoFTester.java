010f24d0dc574f57757f7e250f7f7d183afd48f0
Esther Kim <ekim30@stuy.edu>
2021-09-23 21:54:25 -0400

    Initial Setup

---
+++ CtoFTester.java
@@ -0,0 +1,16 @@
+/*
+ The funtions should have numbers as parameters, the
+ temperature and an indicator that lets us know if
+ it is in celsius or fahrenheit.
+ The parameter for temperature should be a double,
+ the parameter for type should be integer.
+ The functions should return a double.
+*/
+
+public class CtoFTester {
+  public static void main (String args[]) {
+    
+  }
+
+  public static
+}
